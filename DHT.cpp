b543fcd17419f26bb91e0fce6f2833ea04e1bbc3
Yang Lei <yltt1234512@gmail.com>
2013-07-28 13:32:05 +0800

    Update DHT.cpp

--- DHT.cpp
+++ DHT.cpp
@@ -107,7 +107,7 @@ boolean DHT::read(void) {
   pinMode(_pin, OUTPUT);
   digitalWrite(_pin, LOW);
   delay(20);
-  cli();
+  noInterrupts();
   digitalWrite(_pin, HIGH);
   delayMicroseconds(40);
   pinMode(_pin, INPUT);
@@ -137,7 +137,7 @@ boolean DHT::read(void) {
 
   }
 
-  sei();
+  interrupts();
   
   /*
   Serial.println(j, DEC);
