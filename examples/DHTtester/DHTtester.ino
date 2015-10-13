572347f137c08ae7a8d723ddcfc8b7a3149afaed
Tony DiCola <tony@tonydicola.com>
2015-10-12 21:09:17 -0700

    Merge comment update from pull #43

--- examples/DHTtester/DHTtester.ino
+++ examples/DHTtester/DHTtester.ino
@@ -3,11 +3,11 @@
 
 #include "DHT.h"
 
-#define DHTPIN 2     // what pin we're connected to
+#define DHTPIN 2     // what digital pin we're connected to
 
 // Uncomment whatever type you're using!
 //#define DHTTYPE DHT11   // DHT 11
-#define DHTTYPE DHT22   // DHT 22  (AM2302)
+#define DHTTYPE DHT22   // DHT 22  (AM2302), AM2321
 //#define DHTTYPE DHT21   // DHT 21 (AM2301)
 
 // Connect pin 1 (on the left) of the sensor to +5V
