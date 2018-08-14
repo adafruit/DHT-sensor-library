7787bdd18674a25692c35d2e6fffd944d90f3d9a
Gustavo Ambrozio <gustavo@gustavo.eng.br>
2018-08-14 13:44:40 -0600

    Making the library work nicely with the nrf52

--- DHT.h
+++ DHT.h
@@ -64,10 +64,14 @@ class DHT {
 class InterruptLock {
   public:
    InterruptLock() {
+#if !defined(ARDUINO_ARCH_NRF52)  
     noInterrupts();
+#endif
    }
    ~InterruptLock() {
+#if !defined(ARDUINO_ARCH_NRF52)  
     interrupts();
+#endif
    }
 
 };
