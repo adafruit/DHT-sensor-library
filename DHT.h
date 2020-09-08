fa22bc6ca4ce9f5f8911400267e762cecd524e98
Tom Petz <tom@tjpetz.com>
2020-09-08 14:41:40 -0400

    Fix lint errors

--- DHT.h
+++ DHT.h
@@ -47,10 +47,11 @@
 #define DHT21 21  /**< DHT TYPE 21 */
 #define AM2301 21 /**< AM2301 */
 
-#if (TARGET_NAME==ARDUINO_NANO33BLE)
-// As of 7 Sep 2020 the Arduino Nano 33 BLE boards do not have microsecondsToClockCycles defined.
+#if (TARGET_NAME == ARDUINO_NANO33BLE)
+// As of 7 Sep 2020 the Arduino Nano 33 BLE boards do not have 
+// microsecondsToClockCycles defined.
 #ifndef microsecondsToClockCycles
-#define microsecondsToClockCycles(a) ( (a) * (SystemCoreClock / 1000000L) )
+#define microsecondsToClockCycles(a) ((a) * (SystemCoreClock / 1000000L))
 #endif
 #endif
 
