3c9c6604614ada826fc37c8304442b13cb219445
Doxygen CI <travis@travis-ci.org>
2020-09-08 20:38:54 +0000

    Deploy code docs to GitHub Pages Travis build:
    Commit:

--- html/search/defines_2.js
+++ html/search/defines_2.js
@@ -1,4 +1,5 @@
 var searchData=
 [
+  ['microsecondstoclockcycles',['microsecondsToClockCycles',['../_d_h_t_8h.html#aa4a5bbc71d71ab25856c1366b6ca15bb',1,'DHT.h']]],
   ['min_5finterval',['MIN_INTERVAL',['../_d_h_t_8cpp.html#ae5d44a5fa9fd9d113f6bff639f06ddd6',1,'DHT.cpp']]]
 ];
