d9f1e4e0593f0a7af9ca91ee96f859109384b227
ltruelove <levitruelove@gmail.com>
2022-07-17 09:43:59 -0400

    Add comments to the new methods

--- DHT.cpp
+++ DHT.cpp
@@ -72,10 +72,20 @@ void DHT::begin(uint8_t usec) {
   pullTime = usec;
 }
 
+/*!
+ *  @brief  Sets the _pin property value
+ *  @param  newPin
+ *          pin number that sensor is connected
+ */
 void DHT::setPin(uint8_t newPin){
 	_pin = newPin;
 }
 
+/*!
+ *  @brief  Sets the _type property value
+ *  @param  newType
+ *          type of sensor
+ */
 void DHT::setType(uint8_t newType){
 	_type = newType;
 }
