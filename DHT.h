8c79f8d0fe10b789eb43ba44f1b194bf1fc221b0
piclaw <piclaw@localhost>
2026-02-05 09:42:48 -0500

    Fix macro formatting for CI clang-format version

--- DHT.h
+++ DHT.h
@@ -29,21 +29,15 @@
 
 /* Setup debug printing macros. */
 #ifdef DHT_DEBUG
-#define DEBUG_PRINT(...)              \
-  {                                   \
-    DEBUG_PRINTER.print(__VA_ARGS__); \
-  }
-#define DEBUG_PRINTLN(...)              \
-  {                                     \
-    DEBUG_PRINTER.println(__VA_ARGS__); \
-  }
+#define DEBUG_PRINT(...) \
+  { DEBUG_PRINTER.print(__VA_ARGS__); }
+#define DEBUG_PRINTLN(...) \
+  { DEBUG_PRINTER.println(__VA_ARGS__); }
 #else
 #define DEBUG_PRINT(...) \
-  {                      \
-  } /**< Debug Print Placeholder if Debug is disabled */
+  {} /**< Debug Print Placeholder if Debug is disabled */
 #define DEBUG_PRINTLN(...) \
-  {                        \
-  } /**< Debug Print Line Placeholder if Debug is disabled */
+  {} /**< Debug Print Line Placeholder if Debug is disabled */
 #endif
 
 /* Define types of sensors. */
